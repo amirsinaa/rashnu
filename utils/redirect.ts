4c0f3cc2ae70848f6aba5727a0bfd9904de1cf32
amirsina <amirsina23@gmail.com>
2022-01-02 12:14:12 +0330

    - added redirect utility.

---
+++ utils/redirect.ts
@@ -0,0 +1,11 @@
+import Router from 'next/router';
+import { NextPageContext } from 'next';
+
+export const redirect = (target: string, context?: NextPageContext) => {
+  if (context?.res) {
+    context.res.writeHead(303, { Location: target });
+    context.res.end();
+  } else {
+    Router.replace(target);
+  }
+};
