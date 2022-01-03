7abed50ad2a38f69e68c2f56b75823844f6233f4
amirsina <amirsina23@gmail.com>
2022-01-03 17:31:29 +0330

    - use shadertoy component.

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,8 +7,10 @@ import {
 } from '@chakra-ui/react';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import rashnuBlack from '../public/static/images/rashnu-black.png';
+import Shadertoy from "@/modules/shadertoy/Shadertoy"
 import { useTranslation } from 'next-i18next'
 import { useRouter } from 'next/router'
+import { Fragment } from "react";
 import { NextPage } from 'next';
 import Image from 'next/image';
 
@@ -18,49 +20,53 @@ const IndexPage: NextPage = () => {
   const { locale } = useRouter();
 
   return (
-    <Center py={6} mt={'10%'}>
-      <Box
-        maxW={'445px'}
-        w={'full'}
-        boxShadow={'2xl'}
-        rounded={'md'}
-        p={6}
-        overflow={'hidden'}>
+    <Fragment>
+      <Shadertoy />
+      <Center py={6} mt={'10%'}>
         <Box
-          bg={'gray.100'}
-          mt={-6}
-          mx={-6}
-          mb={6}
-          p={10}
-          justifyContent={'center'}
-          display={'flex'}
-          pos={'relative'}
-        >
-          <Image
-            src={rashnuBlack}
-            alt={'Rashni'}
-            width={'150px'}
-            height={'150px'}
-          />
+          maxW={'445px'}
+          w={'full'}
+          boxShadow={'2xl'}
+          className='slogan-box'
+          rounded={'md'}
+          p={6}
+          overflow={'hidden'}>
+          <Box
+            bg={'gray.100'}
+            mt={-6}
+            mx={-6}
+            mb={6}
+            p={10}
+            justifyContent={'center'}
+            display={'flex'}
+            pos={'relative'}
+          >
+            <Image
+              src={rashnuBlack}
+              alt={'Rashni'}
+              width={'150px'}
+              height={'150px'}
+            />
+          </Box>
+          <Stack>
+            <Text
+              color={'green.500'}
+              textTransform={'uppercase'}
+              fontWeight={800}
+              fontSize={'sm'}
+              letterSpacing={1.1}>
+              {locale === 'fa' ? <a href="/">EN</a> : <a href="/fa">FA</a>}
+            </Text>
+            <Heading
+              fontSize={'2xl'}
+              fontFamily={'body'}
+              textAlign={'center'}>
+              {t('RASHNU')}
+            </Heading>
+          </Stack>
         </Box>
-        <Stack>
-          <Text
-            color={'green.500'}
-            textTransform={'uppercase'}
-            fontWeight={800}
-            fontSize={'sm'}
-            letterSpacing={1.1}>
-            {locale === 'fa' ? <a href="/">EN</a> : <a href="/fa">FA</a>}
-          </Text>
-          <Heading
-            fontSize={'2xl'}
-            fontFamily={'body'}
-            textAlign={'center'}>
-            {t('RASHNU')}
-          </Heading>
-        </Stack>
-      </Box>
-    </Center>
+      </Center>
+    </Fragment>
   );
 };
 export const getStaticProps = async ({ locale }) => ({
